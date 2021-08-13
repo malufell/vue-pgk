2d6dbea29b90a30e9fe0099b4578ff7c6932c32d
Malu <malufell@hotmail.com>
2021-08-13 14:49:21 -0300

    refactor: add name property to routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import Posts from '../views/Posts/';
 import PostsEdit from '../views/Posts/PostsEdit';
 
 export const routes = [
-    { path: '/posts', component: Posts },
-    { path: '/postsEdit', component: PostsEdit },
+    { path: '/posts', name: 'posts', component: Posts },
+    { path: '/postsEdit/:id', name: 'edit', component: PostsEdit },
     { path: '*', component: Posts },
 ];
