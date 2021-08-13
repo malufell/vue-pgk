d7645d5659df2587cbc4c77288f5b34c400caf07
Malu <malufell@hotmail.com>
2021-08-13 10:01:21 -0300

    refactor: extracts the page template into a component

---
+++ src/components/template/ index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="template">
+    <menu-template></menu-template>
+  
+    <content-template>
+      <header-template></header-template>
+      <slot></slot>
+    </content-template>
+  </div>
+</template>
+
+<script>
+  import Menu from "../../components/template/Menu"
+  import Header from "../../components/template/Header"
+  import Content from "../../components/template/Content"
+
+  export default {
+    components: {
+      "menu-template": Menu,
+      "header-template": Header,
+      "content-template": Content,
+    },
+  }
+</script>
+
+<style lang="scss">
+
+  .template {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    display: grid;
+    grid-template-areas: 
+      "menu content";
+    grid-template-columns: $menu-width-desktop auto;
+    grid-template-rows: 100vh;
+    grid-gap: $gap;
+
+    @media (max-width: $breakpoint-small) {
+      grid-gap: 0;
+      grid-template-columns: 100%;
+      grid-template-rows: $menu-height-mobile 100vh;
+      grid-template-areas: 
+        "menu" 
+        "content";
+    }
+  }
+</style>
--- src/views/Posts/index.vue
+++ src/views/Posts/index.vue
@@ -1,27 +1,20 @@
 <template>
-  <div class="posts">
-    <menu-template></menu-template>
-  
-    <content-template>
-      <header-template></header-template>
-      <card-header></card-header>
-      <card-item v-for="post of filterTopPosts" :key="post.id" :id="post.id" :title="post.title"></card-item>
-    </content-template>
-  </div>
+
+  <page-template>
+    <card-header></card-header>
+    <card-item v-for="post of filterTopPosts" :key="post.id" :id="post.id" :title="post.title"></card-item>
+  </page-template>
+
 </template>
 
 <script>
-  import Menu from "../../components/template/Menu"
-  import Header from "../../components/template/Header"
-  import Content from "../../components/template/Content"
+  import Template from "../../components/template/ index.vue"
   import CardHeader from "../../components/shared/CardHeader"
   import CardItem from "../../components/shared/CardItem"
 
   export default {
     components: {
-      "menu-template": Menu,
-      "header-template": Header,
-      "content-template": Content,
+      "page-template": Template,
       "card-header": CardHeader,
       "card-item": CardItem,
     },
@@ -45,27 +38,8 @@
       }
     }
   }
-
 </script>
 
 <style lang="scss">
 
-  .posts {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    display: grid;
-    grid-template-areas: 
-      "menu content";
-    grid-template-columns: $menu-width-desktop auto;
-    grid-template-rows: 100vh;
-    grid-gap: $gap;
-
-    @media (max-width: $breakpoint-small) {
-      grid-gap: 0;
-      grid-template-columns: 100%;
-      grid-template-rows: $menu-height-mobile 100vh;
-      grid-template-areas: 
-        "menu" 
-        "content";
-    }
-  }
 </style>
